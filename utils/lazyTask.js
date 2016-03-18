1ac4dfd6dbf4566cc1774df044200fef6871a39f
Kirill Vercetti <office@kyzima-spb.com>
2016-03-19 01:19:20 +0300

    +1

--- utils/lazyTask.js
+++ utils/lazyTask.js
@@ -1,11 +1,14 @@
 'use strict';
 
+const Path = require('path');
+const gulp = require('gulp');
+
 
 function lazyTask(name, path, options) {
     options = options || {};
     
     gulp.task(name, function (cb) {
-        let task = require(path).call(this, options);
+        let task = require(Path.resolve(path)).call(this, options);
         return task(cb);
     });
 }
