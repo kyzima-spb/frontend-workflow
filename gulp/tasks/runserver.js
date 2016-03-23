29a1ec62f26e42a9370c89a5a7808fd8fb6937f1
Kirill Vercetti <office@kyzima-spb.com>
2016-03-23 11:39:14 +0300

    Added runserver task

---
+++ gulp/tasks/runserver.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const exec = require('child_process').exec;
+
+
+module.exports = function (config) {
+    return function (done) {
+        let proc = exec('PYTHONUNBUFFERED=1 python ' + config.manage + ' runserver');
+
+        if (config.stdout) {
+            proc.stdout.on('data', function (data) {
+                process.stdout.write(data);
+            });
+        }
+
+        if (config.stderr) {
+            proc.stderr.on('data', function (data) {
+                process.stdout.write(data);
+            });
+        }
+        
+        done();
+    }
+};
