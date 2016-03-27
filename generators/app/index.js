281ed5c6b60fa1d4a87976cea31d8ae19b8cef96
Kirill Vercetti <office@kyzima-spb.com>
2016-03-28 00:11:04 +0300

    First step: copy main files + configs

--- generators/app/index.js
+++ generators/app/index.js
@@ -19,6 +19,34 @@ module.exports = yeoman.generators.Base.extend({
         let prompts = [
                 {
                     type: 'confirm',
+                    name: 'hasDjango',
+                    message: 'Is Django?',
+                    default: false,
+                    when: function (answers) {
+                        answers.hasDjango = false;
+                        return this.fs.exists(this.destinationPath('manage.py'));
+                    }.bind(this)
+                },
+                {
+                    type: 'input',
+                    name: 'django_host',
+                    message: 'Enter a host that is running Django:',
+                    default: 'localhost',
+                    when: function (answers) {
+                        return answers.hasDjango;
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'django_port',
+                    message: 'Enter a port that is running Django:',
+                    default: '8000',
+                    when: function (answers) {
+                        return answers.hasDjango;
+                    }
+                },
+                {
+                    type: 'confirm',
                     name: 'hasBootstrap',
                     message: 'You want to use a Bootstrap?',
                     default: false
@@ -30,15 +58,6 @@ module.exports = yeoman.generators.Base.extend({
                     default: false
                 }
             ];
-            
-        //if (this.fs.exists(this.destinationPath('manage.py'))) {
-            prompts.push({
-                type: 'confirm',
-                name: 'hasDjango',
-                message: 'Is Django?',
-                default: false
-            });
-        //}
 
         this.prompt(prompts, function (answers) {
             this.answers = answers;
@@ -53,11 +72,17 @@ module.exports = yeoman.generators.Base.extend({
         },
         
         configFiles: function () {
-            let context = _.pick(this.answers, ['hasDjango']),
-                options = { delimiter: '$' };
-            
-            this.template('config/config.json', 'config.json', context, options);
-            this.template('config/config-dev.json', 'config-dev.json', context, options);
+            let options = { delimiter: '$' },
+                dir = this.answers.hasDjango ? 'django' : 'simple';
+                
+            this.template(`config/${dir}/_config.json`, 'config.json', this.answers, options);
+            this.template(`config/${dir}/_config-dev.json`, 'config-dev.json', this.answers, options);
+        },
+        
+        npm: function () {
+            this.template('_package.json', 'package.json', {
+                package_name: _.kebabCase(this.appname)
+            });
         },
         
         bower: function () {
@@ -97,8 +122,6 @@ module.exports = yeoman.generators.Base.extend({
     },
 
     install: function () {
-        this.installDependencies({
-            npm: false
-        });
+        this.installDependencies();
     }
 });
